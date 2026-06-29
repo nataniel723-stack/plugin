2cdb2592e37ba0aeea450fbc5e9f6277e23657f4
nataniel723-stack <nataniel723@gmail.com>
2026-06-29 17:50:14 +0500

    Update embylampa.js

--- embylampa.js
+++ embylampa.js
@@ -2,7 +2,7 @@
     'use strict';
 
     const PLUGIN_NAME = 'Emby';
-    const PLUGIN_VERSION = '0.9.2'; // Обновил версию
+    const PLUGIN_VERSION = '0.9.1'; // Обновил версию
 
     const STORAGE_URL = 'emby_url';
     const STORAGE_API_KEY = 'emby_api_key';
