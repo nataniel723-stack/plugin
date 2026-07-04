bb19cb57e3dfa400e56f402336e76b38565455ce
nataniel723-stack <nataniel723@gmail.com>
2026-07-04 10:48:27 +0500

    Update emby.js

--- emby.js
+++ emby.js
@@ -2,7 +2,7 @@
     'use strict';
 
     const PLUGIN_NAME = 'Emby';
-    const PLUGIN_VERSION = '0.9';
+    const PLUGIN_VERSION = '0.9.8';
 
     const STORAGE_URL = 'emby_url';
     const STORAGE_API_KEY = 'emby_api_key';
