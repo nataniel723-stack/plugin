9eec7c597f5856ba119dc8f26762f5575be461c2
nataniel723-stack <nataniel723@gmail.com>
2026-07-07 22:23:11 +0500

    Update emby.js

--- emby.js
+++ emby.js
@@ -2,7 +2,7 @@
     'use strict';
 
     const PLUGIN_NAME = 'Emby';
-    const PLUGIN_VERSION = '0.9.8';
+    const PLUGIN_VERSION = '0.9.9';
 
     const STORAGE_URL = 'emby_url';
     const STORAGE_API_KEY = 'emby_api_key';
@@ -170,7 +170,7 @@
         let container = item.Container ? item.Container.split(',')[0] : 'mp4';
         
         let playObj = {
-            title: movie ? (movie.title || movie.name || item.Name) : item.Name,
+            title: movie.name,
             url: buildStreamUrl(item.Id, container),
             poster: item.PrimaryImageTag ? `${base}/Items/${item.Id}/Images/Primary?tag=${item.PrimaryImageTag}` : '',
             timeline: Lampa.Timeline.view(timelineKey),
