f8173a0f40f6217e850c3aa4014b31af61ae57bf
nataniel723-stack <nataniel723@gmail.com>
2026-08-28 01:25:08 +0500

    Update rate.js

--- rate.js
+++ rate.js
@@ -356,14 +356,15 @@
 				var render = e.object.activity.render();
                 
                 // Обрабатываем TMDB
-                var tmdbBlock = $('.rate--tmdb', render);
-                if (tmdbBlock.length) {
-                    tmdbBlock.removeClass('rate').addClass('monochrome-rate-custom'); 
-                    var tmdbIcon = tmdbBlock.find('> div:eq(1)');
-                    if (tmdbIcon.find('svg').length === 0) {
-                        tmdbIcon.html('<svg width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M6 4 C 4.9 4 4 4.9 4 6 V 17 C 4 18.1 4.9 19 6 19 H 7 V 22 L 11 19 H 18 C 19.1 19 20 18.1 20 17 V 6 C 20 4.9 19.1 4 18 4 Z"/><text x="12" y="11.5" font-size="6.5" font-family="sans-serif" font-weight="900" text-anchor="middle" fill="currentColor" stroke="none">TM</text><text x="12" y="17.5" font-size="6.5" font-family="sans-serif" font-weight="900" text-anchor="middle" fill="currentColor" stroke="none">DB</text></svg>');
-                    }
-                }
+var tmdbBlock = $('.rate--tmdb', render);
+if (tmdbBlock.length) {
+    tmdbBlock.removeClass('rate').addClass('monochrome-rate-custom'); 
+    var tmdbIcon = tmdbBlock.find('> div:eq(1)');
+    if (tmdbIcon.find('svg').length === 0) {
+        tmdbIcon.html('<svg width="45" height="45" viewBox="0 0 24 24" fill="currentColor"><path d="M19.491 21.899c2.106 0 3.531-1.424 3.531-3.531V3.531C23.022 1.425 21.598 0 19.491 0H4.509C2.403 0 .978 1.424.978 3.531V24l1.809-2.101V3.531a1.721 1.721 0 0 1 1.719-1.719h14.982c.949.002 1.718.77 1.719 1.719v14.837a1.721 1.721 0 0 1-1.719 1.719H6.92l-1.81 1.812-.011-.014zM8.787 11.466H7.09v5.698h1.697c3.793 0 3.793-5.698 0-5.698zm0 4.559h-.551v-3.419h.551c2.215 0 2.215 3.418 0 3.418zM8.456 10.389h1.139V5.83h1.418V4.699H7.037V5.83h1.419v4.559zM14.063 7.201l-1.971-2.502h-.366v5.785h1.156v-3.18l1.182 1.531 1.183-1.531-.008 3.18h1.156V4.699h-.36l-1.971 2.502zM15.983 14.315c.358-.247.51-.689.526-1.124.023-1.004-.606-1.729-1.617-1.729h-2.255v5.706h2.255a1.695 1.695 0 0 0 1.681-1.694v-.02-.008c0-.466-.231-.878-.585-1.127l-.004-.003zm-2.204-1.714h1.013c.327 0 .526.255.526.573a.533.533 0 0 1-.526.574h-1.013V12.6zm1.013 3.427h-1.013v-1.139h1.027c.309 0 .559.25.559.559v.014a.566.566 0 0 1-.566.566h-.001z"/></svg>');
+    }
+}
+
 
                 // Обрабатываем IMDb
                 var imdbBlock = $('.rate--imdb', render);
@@ -376,7 +377,6 @@
                 }
 
                 // Идеальный Кинопоиск: равный угловой шаг между всеми 5 лучами
-// Замените только этот блок для Кинопоиска в функции startPlugin:
 var kpBlock = $('.rate--kp', render);
 if (kpBlock.length) {
     kpBlock.removeClass('rate').addClass('monochrome-rate-custom'); 
